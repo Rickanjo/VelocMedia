833daebcc161ed5b55f331274ee2ede0f407cd4d
Rickanjo <Ricardo.cassettari07@gmail.com>
2023-04-07 21:14:40 -0300

    Exercício Resolvido

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// Calcule a velocidade, a partir da distância (Δd, em metros) e do tempo (Δt, em segundos).
+
+double distancia;
+double tempo;
+double velocidade;
+
+Console.WriteLine("--- Cálculo de Velocidade ---\n");
+
+Console.Write("Digite a distância (em metros): ");
+distancia = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Digite o tempo (em segundos): ");
+tempo = Convert.ToDouble(Console.ReadLine());
+
+velocidade = distancia / tempo;
+
+Console.WriteLine($"\nVelocidade: {velocidade:F2} m/s");
